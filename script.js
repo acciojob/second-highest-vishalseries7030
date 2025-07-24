c688fdcd0a0cdbb4524bbfd07b4aab341b130522
vishal payyawar <139535850+vishalseries7030@users.noreply.github.com>
2025-07-24 12:12:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+  const unique = [...new Set(arr)];
+  if (unique.length < 2) return -Infinity;
+  unique.sort((a, b) => b - a);
+  return unique[1];
+}
