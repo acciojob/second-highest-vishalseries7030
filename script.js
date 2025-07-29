938e68a6de16f5ad203ca4c524e4016dfc03e934
vishal payyawar <139535850+vishalseries7030@users.noreply.github.com>
2025-07-29 10:40:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 function secondHighest(arr) {
-  let max = -Infinity;
-  let second = -Infinity;
+	let max = -Infinity;
+	let second = -Infinity;
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > max) {
-      second = max;
-      max = arr[i];
-    } else if (arr[i] > second && arr[i] < max) {
-      second = arr[i];
-    }
-  }
+	for (let i = 0; i < arr.length; i++) {
+		arr[i] = Number(arr[i]); // convert string to number
 
-  if (second === -Infinity) return -Infinity;
-  return second;
+		if (arr[i] > max) {
+			second = max;
+			max = arr[i];
+		} else if (arr[i] > second && arr[i] < max) {
+			second = arr[i];
+		}
+	}
+
+	return second === -Infinity ? -Infinity : second;
 }
+
